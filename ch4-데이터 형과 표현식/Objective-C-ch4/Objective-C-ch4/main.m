3796eb576162ad9f278a274b0745154b69614ca5
hyun99999 <ypsophila97@icloud.com>
2022-06-18 12:49:24 +0900

    feat: 형 변환 연산자 (#3)

--- ch4-데이터 형과 표현식/Objective-C-ch4/Objective-C-ch4/main.m
+++ ch4-데이터 형과 표현식/Objective-C-ch4/Objective-C-ch4/main.m
@@ -14,11 +14,21 @@ int main(int argc, const char * argv[]) {
         double doubleVar = 8.44e+11;
         char charVar = 'W';
         
+        // intergerVar = 100
         NSLog(@"intergerVar = %i", intergerVar);
+        // floatingVar = 331.790009
         NSLog(@"floatingVar = %f", floatingVar);
+        // doubleVar = 8.440000e+11
         NSLog(@"doubleVar = %e", doubleVar);
+        // doubleVar = 8.44e+11
         NSLog(@"doubleVar = %g", doubleVar);
+        // charVar = W
         NSLog(@"charVar = %c", charVar);
+        
+        // 51
+        NSLog(@"%i", (int)(29.55 + 21.99));
+        // 50
+        NSLog(@"%i", (int)29.55 + (int)21.99);
     }
     return 0;
 }
