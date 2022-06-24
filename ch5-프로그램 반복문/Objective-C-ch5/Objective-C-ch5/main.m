97282c094e1eacf29eebfae7f3201180a0fa4edc
hyun99999 <ypsophila97@icloud.com>
2022-06-24 21:30:51 +0900

    feat: 삼각수 표를 생성하는 프로그램 for 문으로 작성 (#5)

--- ch5-프로그램 반복문/Objective-C-ch5/Objective-C-ch5/main.m
+++ ch5-프로그램 반복문/Objective-C-ch5/Objective-C-ch5/main.m
@@ -7,10 +7,22 @@
 
 #import <Foundation/Foundation.h>
 
+// 삼각수 표를 생성하는 프로그램
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int n, triangularNumber;
+        
+        NSLog(@"TABLE OF TRIANGULAR NUMBERS");
+        NSLog(@"N Sum from 1 to n");
+        NSLog(@"-- ----------------");
+        
+        triangularNumber = 0;
+        
+        for(n = 1; n <= 10; n++) {
+            triangularNumber += n;
+            NSLog(@"%i       %i", n, triangularNumber);
+        }
     }
     return 0;
 }
