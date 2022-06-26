af736ae8b8f649a2749f2168b7f6c6ef6aa78deb
hyun99999 <ypsophila97@icloud.com>
2022-06-26 17:08:49 +0900

    chore: project setting (#7)

---
+++ ch6-의사결정하기/Objective-C-ch6/Objective-C-ch6/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C-ch6
+//
+//  Created by kimhyungyu on 2022/06/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
