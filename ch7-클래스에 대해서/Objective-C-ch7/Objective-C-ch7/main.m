6df93c8b34fd5227cc3a4c016b2aea69fcec3efa
hyun99999 <ypsophila97@icloud.com>
2022-06-30 06:38:16 +0900

    chore: project setting (#9)

---
+++ ch7-클래스에 대해서/Objective-C-ch7/Objective-C-ch7/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C-ch7
+//
+//  Created by kimhyungyu on 2022/06/30.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
