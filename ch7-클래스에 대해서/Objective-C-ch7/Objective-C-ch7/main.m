6dd04dbb6806a06340b1515765afccf284282161
hyun99999 <ypsophila97@icloud.com>
2022-07-01 05:13:07 +0900

    feat: . 연산자를 사용하여 프로퍼티 접근 (#9)

--- ch7-클래스에 대해서/Objective-C-ch7/Objective-C-ch7/main.m
+++ ch7-클래스에 대해서/Objective-C-ch7/Objective-C-ch7/main.m
@@ -5,12 +5,17 @@
 //  Created by kimhyungyu on 2022/06/30.
 //
 
-#import <Foundation/Foundation.h>
+#import "Fraction/Fraction.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Fraction *myFraction = [[Fraction alloc] init];
+        
+        myFraction.numerator = 1;
+        myFraction.denominator = 2;
+        
+        NSLog(@"The numerator is %i, and denominator is %i", myFraction.numerator, myFraction.denominator);
     }
+      
     return 0;
 }
