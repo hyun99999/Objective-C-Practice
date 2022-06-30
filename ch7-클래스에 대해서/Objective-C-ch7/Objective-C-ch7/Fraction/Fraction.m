b9c983e4b670fdeb580ede11c546cfde1106a0ee
hyun99999 <ypsophila97@icloud.com>
2022-07-01 03:15:14 +0900

    refactor: @synthesize 대신 @property만으로 게터,세터 생성 (#9)
    - Xcode 4.5 부터 @synthesize 지시어를 사용하지 않아도 된다. @property 지시어만으로도 컴파일러는 자동으로 세터와 게터를 생성해 준다.

--- ch7-클래스에 대해서/Objective-C-ch7/Objective-C-ch7/Fraction/Fraction.m
+++ ch7-클래스에 대해서/Objective-C-ch7/Objective-C-ch7/Fraction/Fraction.m
@@ -15,15 +15,15 @@
 
 // synthesize 는 프로퍼티에 해당하는 getter, setter 메서드를 생성하도록 지시한다.
 // 컴파일러가 작성한 접근자 메서드는 효율적이고, 다중 스레드, 다양한 시스템, 다중 코어 환경에서 좀 더 안전하게 작동합니다.
-@synthesize numerator, denominator;
+//@synthesize numerator, denominator;
 
 - (void)print {
-    NSLog(@"%i/%i", numerator, denominator);
+    NSLog(@"%i/%i", self.numerator, self.denominator);
 }
 
 - (double)convertToNum {
-    if (denominator != 0)
-        return (double)numerator / denominator;
+    if (_denominator != 0)
+        return (double)_numerator / _denominator;
     else
         return NAN;
 }
