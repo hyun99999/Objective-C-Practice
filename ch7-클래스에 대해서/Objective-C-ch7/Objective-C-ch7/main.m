eb32b01f9108d9cd51aaac0bee716cf7f700eeff
hyun99999 <ypsophila97@icloud.com>
2022-07-01 05:18:09 +0900

    comment: . 연산자와 메시지 형식 주석 추가 (#9)

--- ch7-클래스에 대해서/Objective-C-ch7/Objective-C-ch7/main.m
+++ ch7-클래스에 대해서/Objective-C-ch7/Objective-C-ch7/main.m
@@ -14,6 +14,7 @@ int main(int argc, const char * argv[]) {
         myFraction.numerator = 1;
         myFraction.denominator = 2;
         
+        // . 연산자는 프로퍼티에 사용하고, 메서드는 대괄호를 사용한 메시지 표현식을 쓰도록 선호되고 있다.
         NSLog(@"The numerator is %i, and denominator is %i", myFraction.numerator, myFraction.denominator);
     }
       
