e3c2f86153c6b4f1c48e2fa1ac8cfc943febe793
hyun99999 <ypsophila97@icloud.com>
2022-06-30 16:04:38 +0900

    feat: implementation 을 구현한 m파일 구현 (#9)

--- ch7-클래스에 대해서/Objective-C-ch7/Objective-C-ch7/Fraction/Fraction.m
+++ ch7-클래스에 대해서/Objective-C-ch7/Objective-C-ch7/Fraction/Fraction.m
@@ -5,4 +5,40 @@
 //  Created by kimhyungyu on 2022/06/30.
 //
 
-#import <Foundation/Foundation.h>
+// 꺾쇠가 아닌 큰 따옴표로 로컬 파일을 임포트한다.
+
+#import "Fraction.h"
+
+@implementation Fraction {
+    int numerator;
+    int denominator;
+}
+
+- (void)print {
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+- (void)setNumerator:(int)n {
+    numerator = n;
+}
+
+- (void)setDenominatr:(int)d {
+    denominator = d;
+}
+
+- (int)numerator {
+    return numerator;
+}
+
+- (int)denominator {
+    return denominator;
+}
+
+- (double)convertToNum {
+    if (denominator != 0)
+        return (double)numerator / denominator;
+    else
+        return NAN;
+}
+
+@end
