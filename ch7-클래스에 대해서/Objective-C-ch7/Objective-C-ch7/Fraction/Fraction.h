ff11267860a96be9f307630335f4441958c6b948
hyun99999 <ypsophila97@icloud.com>
2022-06-30 15:56:06 +0900

    feat: 인터페이스를 선언한 헤더파일 구현 (#9)
    - 인터페이스 부분은 클래스를 다른 파일에서 사용할 때 컴파일러가 필요로 하는 모든 정보를 담고 있다.

---
+++ ch7-클래스에 대해서/Objective-C-ch7/Objective-C-ch7/Fraction/Fraction.h
@@ -0,0 +1,23 @@
+//
+//  Fraction.h
+//  Objective-C-ch7
+//
+//  Created by kimhyungyu on 2022/06/30.
+//
+
+#import <Foundation/Foundation.h>
+
+// MARK: -  Fraction Class
+
+// interface 부분은 클래스를 다른 파일에서 사용할 때 컴파일러가 필요로 하는 모든 정보를 담고 있다.
+
+@interface Fraction : NSobject
+
+- (void) print;
+- (void) setNumerator:(int)n;
+- (void) setDenominatr:(int)d;
+- (int) numerator;
+- (int) denominator;
+- (double) convertToNum;
+
+@end
