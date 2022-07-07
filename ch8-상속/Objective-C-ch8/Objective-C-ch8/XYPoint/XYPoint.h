d6970e3f38d6a1e44e0c828dd6855a0aad8cd309
hyun99999 <ypsophila97@icloud.com>
2022-07-07 15:07:10 +0900

    add: XYPoint 헤더파일 추가 (#11)
    - x,y 좌표를 저장하기 위한 목적

---
+++ ch8-상속/Objective-C-ch8/Objective-C-ch8/XYPoint/XYPoint.h
@@ -0,0 +1,15 @@
+//
+//  XYPoint.h
+//  Objective-C-ch8
+//
+//  Created by kimhyungyu on 2022/07/07.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYPoint : NSObject
+
+@property int x, y;
+
+- (void)setX:(int)xVal andY:(int)yVal;
+@end
