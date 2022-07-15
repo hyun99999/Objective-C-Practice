44c8612bae0aa459e36f00613c8b2b04387d4ad8
hyun99999 <ypsophila97@icloud.com>
2022-07-15 10:47:41 +0900

    chore: project setting (#13)

---
+++ ch9-다형성, 동적 타이핑, 동적 바인딩/Objective-C-ch9/Objective-C-ch9/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C-ch9
+//
+//  Created by kimhyungyu on 2022/07/15.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
