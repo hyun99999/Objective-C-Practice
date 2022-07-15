1e5bdfac4070c1abb392bfa148534b4394171db6
hyun99999 <ypsophila97@icloud.com>
2022-07-15 11:52:36 +0900

    feat: 런타임시에 메시지의 수신자를 인지 (#13)
    - 다형성을 통해 동일한 메서드 이름에 각각 응답할 수있는 클래스 모음을 개발하는 것이 가능.

--- ch9-다형성, 동적 타이핑, 동적 바인딩/Objective-C-ch9/Objective-C-ch9/Complex.m
+++ ch9-다형성, 동적 타이핑, 동적 바인딩/Objective-C-ch9/Objective-C-ch9/Complex.m
@@ -11,6 +11,8 @@
 
 @implementation Complex
 
+// Xcode 4.5 부터 @synthesize를 사용하지 않아도 @property 지시어만으로도 자동으로 세터와 게터를 생성해줄 수 있다.
+// 해당 인스턴스 변수들은 self. 혹은 _ 을 통해서 사용이 가능하다.
 //@synthesize real, imaginary;
 
 - (void)print {
--- ch9-다형성, 동적 타이핑, 동적 바인딩/Objective-C-ch9/Objective-C-ch9/main.m
+++ ch9-다형성, 동적 타이핑, 동적 바인딩/Objective-C-ch9/Objective-C-ch9/main.m
@@ -6,16 +6,61 @@
 //
 
 #import "Complex.h"
+#import "Fraction.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // alloc: 객체의 데이터를 저장하기에 충분한 메모리 공간이 예약. 즉, 생성.
         // 객체의 모든 인스턴스 변수를 0으로 초기화.
         // init: 인스턴스 초기화. 초기화된 객체를 반환.
-        //
+        // Fraction *f1 = [[Fraction alloc] init];
         // alloc 과 init 을 결합한 new.
+        Fraction *f1 = [Fraction new];
+        Fraction *f2 = [[Fraction alloc] init];
+        Fraction *fracResult;
         
+        Complex *c1 = [[Complex alloc] init];
+        Complex *c2 = [[Complex alloc] init];
+        Complex *compResult;
         
+        [f1 setTo:1 over:10];
+        [f2 setTo:2 over:15];
+        
+        [c1 setReal:18 andImaginary:2.5];
+        [c2 setReal:-5.0 andImaginary:3.2];
+        
+        // 두 개의 복소수를 더하고 결과를 표시한다.
+        [c1 print];
+        NSLog(@"         +");
+        [c2 print];
+        NSLog(@"----------");
+        compResult = [c1 add:c2];
+        [compResult print];
+        NSLog(@"\n");
+        
+        // 두 개의 분수를 더하고 결과를 표시한다.
+        [f1 print];
+        NSLog(@"   +");
+        [f2 print];
+        NSLog(@"----");
+        fracResult = [f1 add:f2];
+        [fracResult print];
     }
     return 0;
 }
+
+/*
+ 
+  18 + 2.5i
+          +
+  -5 + 3.2i
+  ----------
+  13 + 5.7i
+
+ 1/10
+    +
+ 2/15
+ ----
+ 7/30
+ 
+*/
