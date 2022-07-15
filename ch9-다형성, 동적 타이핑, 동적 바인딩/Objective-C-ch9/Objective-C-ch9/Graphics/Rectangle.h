a15ccee92e9253dd0a1e8509ea288a3c36a1ee8b
hyun99999 <ypsophila97@icloud.com>
2022-07-15 17:17:21 +0900

    add: Square, Rectangle, XYPoint 클래스 추가 (#13)
    - 클래스에게 질문하기 위해서 Graphics 관련 클래스 추가하여 사용.

---
+++ ch9-다형성, 동적 타이핑, 동적 바인딩/Objective-C-ch9/Objective-C-ch9/Graphics/Rectangle.h
@@ -0,0 +1,21 @@
+//
+//  Rectangle.h
+//  Objective-C-ch9
+//
+//  Created by kimhyungyu on 2022/07/15.
+//
+
+#import <Foundation/Foundation.h>
+#import "../XYPoint/XYPoint.h"
+
+@interface Rectangle: NSObject
+
+@property int width, height;
+
+- (XYPoint *)origin;
+- (void)setOrigin:(XYPoint *)pt;
+- (int)area;
+- (int)perimeter;
+- (void)setWidth:(int)w andHeight:(int)h;
+
+@end
