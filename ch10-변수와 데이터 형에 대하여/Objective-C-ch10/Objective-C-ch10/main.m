c077af00aea9d21c6116c7eb3aec106f6f8873c5
hyun99999 <ypsophila97@icloud.com>
2022-07-30 22:57:13 +0900

    feat: 외부 변수 예제 (#15)

--- ch10-변수와 데이터 형에 대하여/Objective-C-ch10/Objective-C-ch10/main.m
+++ ch10-변수와 데이터 형에 대하여/Objective-C-ch10/Objective-C-ch10/main.m
@@ -21,3 +21,33 @@ int main(int argc, const char * argv[]) {
     }
     return 0;
 }
+
+// 외부 변수 사용 예제.
+
+#import "Foo.h"
+
+// 외부 변수 선언 및 정의.
+// 정의는 딱 한 번만.
+// 선언은 할당될 저장 공간을 만들지 않지만 정의에서는 공간을 만든다.
+int gGlobalVar = 5;
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        Foo *myFoo = [[Foo alloc] init];
+        NSLog(@"%i", gGlobalVar);
+        // 5
+        
+        [myFoo setgGlobalVar:100];
+        NSLog(@"%i", gGlobalVar);
+        // 100
+    }
+    return 0;
+}
+
+// setgGlobalVar:
+
+- (void)setgGlobalVar:(int)val {
+    // 외부 변수에 접근하기 위해서 extern 키워드를 사용하여 선언.
+    extern int gGlobalVar;
+    gGlobalVar = val;
+}
