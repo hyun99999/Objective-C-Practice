ee058710f562c2e320d00f483cba8b8896f3975a
hyun99999 <ypsophila97@icloud.com>
2022-08-12 00:19:56 +0900

    feat: 카테고리 메소드 사용 (#17)

--- ch11-카테고리와 프로토콜/Objective-C-ch11/Objective-C-ch11/main.m
+++ ch11-카테고리와 프로토콜/Objective-C-ch11/Objective-C-ch11/main.m
@@ -6,11 +6,36 @@
 //
 
 #import "Fraction.h"
+#import "Fraction+MathOps.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Fraction *a = [[Fraction alloc] init];
+        Fraction *b = [[Fraction alloc] init];
+        Fraction *result;
+        
+        [a setTo:1 over:3];
+        [b setTo:2 over:5];
+        
+        [a print]; NSLog(@"  +"); [b print]; NSLog(@"------");
+        result = [a add:b];
+        [result print];
+        NSLog(@"\n");
+        
+        [a print]; NSLog(@"  -"); [b print]; NSLog(@"------");
+        result = [a sub:b];
+        [result print];
+        NSLog(@"\n");
+        
+        [a print]; NSLog(@"  *"); [b print]; NSLog(@"------");
+        result = [a mul:b];
+        [result print];
+        NSLog(@"\n");
+        
+        [a print]; NSLog(@"  /"); [b print]; NSLog(@"------");
+        result = [a div:b];
+        [result print];
+        NSLog(@"\n");
     }
     return 0;
 }
