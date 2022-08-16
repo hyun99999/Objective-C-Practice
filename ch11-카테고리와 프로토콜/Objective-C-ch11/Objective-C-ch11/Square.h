04f30651def8f6d289f70bc562a97adf95d16182
hyun99999 <ypsophila97@icloud.com>
2022-08-16 22:25:42 +0900

    add: 복합객체만들기 위한 파일추가 (#17)
    - 이전 예제와는 달리 복합객체로써 만들 Square 클래스 생성

---
+++ ch11-카테고리와 프로토콜/Objective-C-ch11/Objective-C-ch11/Square.h
@@ -0,0 +1,12 @@
+//
+//  Square.h
+//  Objective-C-ch11
+//
+//  Created by kimhyungyu on 2022/08/16.
+//
+
+#ifndef Square_h
+#define Square_h
+
+
+#endif /* Square_h */
---
+++ ch11-카테고리와 프로토콜/Objective-C-ch11/Objective-C-ch11/Square.m
@@ -0,0 +1,8 @@
+//
+//  Square.m
+//  Objective-C-ch11
+//
+//  Created by kimhyungyu on 2022/08/16.
+//
+
+#import <Foundation/Foundation.h>
