83fe6a71d51b07dbbfdb75f909a7cb1630da3125
hyun99999 <ypsophila97@icloud.com>
2022-08-25 12:12:17 +0900

    add: #define 문 (#18)

--- ch12-전처리기/Objective-C-ch12/Objective-C-ch12/main.m
+++ ch12-전처리기/Objective-C-ch12/Objective-C-ch12/main.m
@@ -7,6 +7,15 @@
 
 #import <Foundation/Foundation.h>
 
+#define TRUE 1
+#define FALSE 0
+
+#define AND &&
+#define OR ||
+#define EQUALS ==
+
+#define TWO_PI 2.0 * PI
+#define PI 3.141592654
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
